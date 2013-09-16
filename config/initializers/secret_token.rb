7b534acfbcd9e94287bf177c418006754e38f003
sokky <phonemic.system@gmail.com>
2013-09-16 15:27:54 +0900

    file add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sorejanai::Application.config.secret_key_base = '91acd6a305b6e13b439c574718af5a03754974bbc102614c39840079d146c9f3ce093357f95698a8f5bb232d9d5ec98165ac459fd40ab68c0ccff45dd1d21295'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sorejanai::Application.config.session_store :cookie_store, key: '_Sorejanai_session'
