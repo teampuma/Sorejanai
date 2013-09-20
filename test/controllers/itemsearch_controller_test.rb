0c3bb75e39d7dc8070a54f7b507379909dccdd99
test <test@com>
2013-09-20 20:00:07 +0900

    add sample test code

---
+++ test/controllers/itemsearch_controller_test.rb
@@ -0,0 +1,24 @@
+# coding: utf-8
+require 'test_helper'
+ 
+class ItemsearchControllerTest  < ActionController::TestCase
+  test "access_index" do
+    # indexにアクセス
+    post(:index)
+    # 200を返却
+    assert_response(:success)
+    # テンプレートが妥当か
+    assert_template('itemsearch/index')
+  end
+
+  test "access_search" do
+    # indexにアクセス
+    post(:search, {:keyword => "うどん"})
+    # 200を返却
+    assert_response(:success)
+    # テンプレートが妥当か
+    assert_template('itemsearch/index')
+    # 入力値がはいっているか
+    assert_select('input[value="うどん"]')
+  end
+end
