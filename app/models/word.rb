74addb920a99eff672660cfaef792ad34d147332
sokky <c@test.com>
2013-10-08 20:18:42 +0900

    test, model fix

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,22 +1,25 @@
 # coding: utf-8
 =begin
-  mecabの辞書ファイルから作成されるCollection
-  mecabの構成は以下の通り
- 工藤,1223,1223,6058,名詞,固有名詞,人名,名,*,*,くどう,クドウ,クドウ  
- 表層形,左文脈ID,右文脈ID,コスト,品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
-
- 表層形、品詞、品詞細分類1-3、原型、読み、発音とアルファベットを対象とする
- アルファベットは格納するときに変換する
+ * 日本語検索用テーブル
+ *  
+ * 定義：
+ * 日本語表示、よみかた（ひらがな）、よみかたの文字数
+ * 
 =end
 class Word
   include Mongoid::Document
   field :surface, type: String
-  field :word_class, type: String
-  field :word_class_detail1, type: String
-  field :word_class_detail2, type: String
-  field :word_class_detail3, type: String
-  field :original, type: String
   field :reading, type: String
-  field :pronunciation, type: String
-  field :alphabet, type: String
+  field :count_hira, type: Integer
+  
+  def self.init
+    # 初期化
+    # ダミーとしてデータを格納する
+    if Word.count == 0 then
+      Word.create(surface: "布団", reading: "ふとん", count_hira:3)
+      Word.create(surface: "牛丼", reading: "ぎゅうどん", count_hira:5)
+      Word.create(surface: "うどん", reading: "うどん", count_hira:3)
+    end
+  end
+  
 end
--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -4,28 +4,41 @@ require 'test_helper'
 class WordTest < ActiveSupport::TestCase
   def setup
     # テストデータの作成
-    Word.create(surface: "test")
   end
   
   def teardown
-    # テストデータの削除
     refs = Word.all
-    # 削除
     refs.delete
   end
 
-  test "find a data" do
-    c = Mongoid::Config
-    
+  test "access mongo" do
+    # テストデータの作成
+    Word.create(surface: "test", reading: "てすと", count_hira:3)
+
     # 参照
     refs = Word.all
     
     # 件数のチェック
-    assert_equal(1, refs.count)
+    assert_equal(1, Word.count)
     refs.each do |ref|
       # 内容のチェック
       assert_equal("test", ref.surface)
+      assert_equal("てすと", ref.reading)
+      assert_equal(3, ref.count_hira)
     end
+
+  end
+  
+  test "init Word Class" do
+    # Wordクラスの初期化=
+    # データが格納されていることを確認
+    Word.init
+
+    # 参照:語尾がどんorとんのものを取得
+    refs = Word.any_in(reading: [/どん$/, /とん$/])
+    
+    # 件数のチェック
+    assert_equal(3, refs.count)
   end
 end
 
