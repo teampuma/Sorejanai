b020f309dc3341a0773c969eae381366d0f962e4
sokky <c@test.com>
2013-10-14 20:48:27 +0900

    delete files

--- app/models/article.rb
+++
@@ -1,4 +0,0 @@
-class Article
-  include Mongoid::Document
-  field :title, type: String
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Sorejanai::Application.routes.draw do
   root 'itemsearch#index'
   post '/search' => 'itemsearch#search'
-  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
