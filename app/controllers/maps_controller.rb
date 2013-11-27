7921bb08690ca0f77fa8a151f017c6e0870cfdd6
sokky <a@test.com>
2013-11-27 08:09:31 -0800

    miss

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -26,7 +26,7 @@ class MapsController < ApplicationController
       refs = Word.get_word_withloc(hira, l[0], l[1])
       @hash = Gmaps4rails.build_markers(refs) do |word, marker|
         # 0,0はゴミデータなので破棄する
-        next if [word.loc[1], word.loc[0]] == [0, 0]
+        # next if [word.loc[1], word.loc[0]] == [0, 0]
         marker.lat word.loc[1]
         marker.lng word.loc[0]
         marker.infowindow word.surface
