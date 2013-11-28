3548a2b4c11fc8de219f3eaa88a033e9584f8c2a
kairiTakemura <thinglikeabar@gmail.com>
2013-11-28 20:56:26 +0900

    add ruby tag at translate

--- app/helpers/translate_helper.rb
+++ app/helpers/translate_helper.rb
@@ -9,7 +9,7 @@ module TranslateHelper
     ret = ""
     res.each do |r|
       if r.changed
-        ret = ret + content_tag(:b, r.chg_surface)
+        ret = ret + content_tag(:ruby) { content_tag(:rb, content_tag(:b, r.chg_surface ) ) + content_tag(:rt, r.chg_reading ) }
       else
         ret = ret + r.surface
       end
